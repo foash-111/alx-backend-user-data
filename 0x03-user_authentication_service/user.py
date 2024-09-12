480148de1421ba35c9544186ac3fddc6508c5340
foash-111 <mr2221@fayoum.edu.eg>
2024-09-12 15:55:46 +0300

    User table ORM schema in database

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,7 @@
 #!/user/bin/env python3
-
+"""our BASE USER model
+our table schema representaion in database
+"""
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
