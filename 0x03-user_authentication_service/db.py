2b3e59c8537cccc520ebbcb3bcd97915fe2953f4
foash-111 <mr2221@fayoum.edu.eg>
2024-09-12 15:59:52 +0300

    echo=False (default) (no SQL logs will be shown)

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -18,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         # echo=False (default) (no SQL logs will be shown)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
