c326f52ae15dd239c5074283088ba77cefc0c307
foash-111 <mr2221@fayoum.edu.eg>
2024-09-16 11:20:53 +0300

    annotate register user, handle exception

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 from uuid import uuid4
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class Auth:
@@ -16,8 +17,10 @@ class Auth:
 
     def register_user(self, email: str = "", password: str = "") -> User:
         """store user with hashed password"""
-        current_user = self._db.find_user_by(email=email)
-        if current_user:
+
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
             raise ValueError(f"User {email} already exists")
         password = _hash_password(password)
         new_user = self._db.add_user(email, password)
