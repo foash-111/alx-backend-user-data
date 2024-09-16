222373b1e7745a81b205ce92dcaff65cf90f8e9e
foash-111 <mr2221@fayoum.edu.eg>
2024-09-16 10:33:58 +0300

    annotate add_user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email="", hashed_password=""):
+    def add_user(self, email: str = "", hashed_password: str = "") -> User:
         """add new user from User class and store it"""
         if email and hashed_password:
             new_user = User(email=email, hashed_password=hashed_password)
